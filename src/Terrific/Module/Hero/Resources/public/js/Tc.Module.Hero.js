06e3d1ee2569bad388efddc5ba0fdc745f2fc2e7
Remo Brunschwiler <remo.brunschwiler@namics.com>
2012-03-04 20:23:53 +0100

    hero demo content

---
+++ src/Terrific/Module/Hero/Resources/public/js/Tc.Module.Hero.js
@@ -0,0 +1,84 @@
+(function($) {
+    /**
+     * Hero module implementation.
+     *
+     * @author Terrific Composer
+     * @namespace Tc.Module
+     * @class Hero
+     * @extends Tc.Module
+     */
+    Tc.Module.Hero = Tc.Module.extend({
+		
+		/**
+		 * Initializes the Hero module.
+		 * 
+		 * @method init
+		 * @return {void}
+	 	 * @constructor
+	     * @param {jQuery} $ctx the jquery context
+	     * @param {Sandbox} sandbox the sandbox to get the resources from
+	     * @param {String} modId the unique module id
+		 */
+		init: function($ctx, sandbox, modId) {
+	      	// call base constructor
+	        this._super($ctx, sandbox, modId);
+	    },
+    
+        /**
+         * Hook function to load the module specific dependencies.
+         *
+         * @method dependencies
+         * @return void
+         */
+        dependencies: function() {
+            // nothing to do here -> please delete unused hooks in real world examples due to performance reasons
+        },
+        
+        /**
+         * Hook function to do module specific stuff before binding the events (i.e. fetching some data).
+         *
+         * @method beforeBinding
+         * @param {Function} callback the callback function which must be called at the end
+         * @return void
+         */
+        beforeBinding: function(callback) {
+            var $ctx = this.$ctx;
+
+            // extract the name and provide the default greeting
+            $('.message', $ctx).val('Hi, I am ' + $('pre', $ctx).data('name'));
+            callback();
+        },
+        
+        /**
+         * Hook function to bind the module specific events.
+         *
+         * @method onBinding
+         * @return void
+         */
+        onBinding: function() {
+            var $ctx = this.$ctx;
+
+            // bind the submit event on the form
+            $('form', $ctx).bind('submit', function() {
+                // write the current message in the bubble
+                $('.bubble', $ctx).text($('.message', $ctx).val());
+
+                return false;
+            })
+        },
+        
+        /**
+         * Hook function to do module specific stuff after binding the events (i.e. triggering some events).
+         *
+         * @method afterBinding
+         * @return void
+         */
+        afterBinding: function() {
+            var $ctx = this.$ctx;
+
+            // trigger the first submit to write the default message in the bubble
+            $('form', $ctx).trigger('submit');
+        }
+        
+    });
+})(Tc.$);
---
+++ src/Terrific/Module/Hero/Resources/public/js/skin/Tc.Module.Hero.Stealth.js
@@ -0,0 +1,67 @@
+(function($) {
+    /**
+     * Stealth Skin implementation for module Hero.
+     *
+     * @author Terrific Composer
+     * @namespace Tc.Module.Hero
+     * @class Stealth
+     * @extends Tc.Module
+     * @constructor
+     */
+    Tc.Module.Hero.Stealth = function(parent) {
+        /** 
+         * override the appropriate methods from the decorated module (ie. this.get = function()).
+         * the former/original method may be called via parent.<method>()
+         */
+        this.beforeBinding = function(callback) {
+            // create mode chooser markup
+            this.$ctx.append($('<div class="mode"><h3>Stealth Mode</h3><a href="#" title="activate stealth mode" class="on">on</a><a href="#" title="deactivate stealth mode" class="off">off</a></div>'));
+
+            // calling parent method
+            parent.beforeBinding(function() {
+                callback();
+            });
+        };
+        
+        this.onBinding = function() {
+            var that = this,
+                $ctx = this.$ctx;
+
+            // binding the stealth mode on / off events
+            $('.on', $ctx).bind('click', function() {
+                that.fire('activateStealthMode', null, function() {
+                    that.onActivateStealthMode();
+                });
+                return false;
+            });
+
+            $('.off', $ctx).bind('click', function() {
+                that.fire('deactivateStealthMode', null, function() {
+                    that.onDeactivateStealthMode();
+                });
+                return false;
+            });
+
+            // calling parent method
+            parent.onBinding();
+        };
+
+        this.onActivateStealthMode = function() {
+            var that = this,
+                $hero = $('pre', this.$ctx);
+
+            $hero.hide(100).show(100).hide(100).show(100).animate({ opacity : 0.2 }, 500, function() {
+                $hero.show(200).hide(200).show(200).hide(200);
+            });
+        };
+
+        this.onDeactivateStealthMode = function() {
+            var that = this,
+                $hero = $('pre', this.$ctx)
+
+            $hero.show(200).hide(200).show(200).animate({ opacity : 1 }, 500, function() {
+                $hero.hide(100).show(100).hide(100).show(100);
+            });
+        };
+    };
+})(Tc.$);
